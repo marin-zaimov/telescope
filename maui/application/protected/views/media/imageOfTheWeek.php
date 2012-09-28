b2a3c476cae5c1dac9a88642f70f1a41f8901a53
Marin Zaimov <marin@gatech.edu>
2012-09-28 01:36:57 -0400

    added controller architecture with necessary views

---
+++ maui/application/protected/views/media/imageOfTheWeek.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this MediaController */
+
+$this->breadcrumbs=array(
+	'Media'=>array('/media'),
+	'ImageOfTheWeek',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
