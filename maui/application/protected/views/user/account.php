a75c5b115608aef74c0a7c264d74cefb6f8ad403
Jasmine Lawrence <jasmine@jasmine-900X3C-900X4C-900X4D.(none)>
2012-10-22 19:42:57 -0400

    updated the registration page with pictures and a video

--- maui/application/protected/views/user/account.php
+++ maui/application/protected/views/user/account.php
@@ -4,25 +4,26 @@
 
 <h1><p><b>Account stuff</b></p></h1>
 
-<p>The Maui Telescope project is a collaboration of the Georgia Institute of Technology and the United States Air force. This project .</p>
+<h3><p><b>My Registrations</b></p></h3>
 
-<p>This is a paragraph about our sponsors.</p>
+<p>This is a list of my reservations dynamically pulled from the calendar!.</p>
+
+<h3><p><b>My Photo's and Video's</b></p></h3>
+
+<p>This is a gallery of all the awesome photo's and video's I took .</p>
+
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/saturn.jpg" alt="saturn"width="200" height="128">
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/sunrise.jpg" alt="sunrise"width="200" height="128">
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/Sun.png" alt="sun"width="200" height="128">
+
+<br>
+<br>
+<br>
+<iframe width="640" height="360" src="http://www.youtube.com/embed/qa04iRjaBMA?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
 
 <p>This site was the Fall 2012 Senior Design project of Team Teamwork. Jasmine Lawrence served as Project Manager. Chris Porter, Marin Zaimov and Brad Vanslyke served as developers.</p>
 
-<h1><p><b>Contact Us</b></p></h1>
 
-<form method="post" enctype="text/plain">
-Name:<br>
-<input type="text" name="name" value="your name"><br>
-E-mail:<br>
-<input type="text" name="mail" value="your email"><br>
-Comment:<br>
-<input type="text" name="comment" value="your comment" size="50"><br><br>
-<input type="submit" value="Send">
-<input type="reset" value="Reset">
-</form>
-</form>
 
 </body>
 </html>
