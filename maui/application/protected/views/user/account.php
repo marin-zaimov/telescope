900120d1a15e770f16595582bfcae00b9a2f5bb3
Jasmine Lawrence <jasmine@jasmine-900X3C-900X4C-900X4D.(none)>
2012-10-22 19:18:09 -0400

    removed old user index

---
+++ maui/application/protected/views/user/account.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1><p><b>Account stuff</b></p></h1>
+
+<p>The Maui Telescope project is a collaboration of the Georgia Institute of Technology and the United States Air force. This project .</p>
+
+<p>This is a paragraph about our sponsors.</p>
+
+<p>This site was the Fall 2012 Senior Design project of Team Teamwork. Jasmine Lawrence served as Project Manager. Chris Porter, Marin Zaimov and Brad Vanslyke served as developers.</p>
+
+<h1><p><b>Contact Us</b></p></h1>
+
+<form method="post" enctype="text/plain">
+Name:<br>
+<input type="text" name="name" value="your name"><br>
+E-mail:<br>
+<input type="text" name="mail" value="your email"><br>
+Comment:<br>
+<input type="text" name="comment" value="your comment" size="50"><br><br>
+<input type="submit" value="Send">
+<input type="reset" value="Reset">
+</form>
+</form>
+
+</body>
+</html>
+
