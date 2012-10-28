e2b746a167456151f1df7e1c9d300a21e4ddbb0a
cporter <cporterbox@gmail.com>
2012-10-28 10:58:50 -0400

    not sure of calendarManager.

--- maui/application/js/calendarManager.js
+++ maui/application/js/calendarManager.js
@@ -10,6 +10,31 @@ console.log(two);
 // CODE IN THIS FUNCTION RUNS ON PAGE LOAD
 $(function() {
   
+  setupCalendar();
+  //enablePopover();
+  
+ 
+  /*
+  $("#close-popover").on("click", function(event) {
+    alert('closer');
+  })
+  */
+
+  
+
+
+});
+
+
+
+// DECLARE FUNCTIONS HERE
+
+
+
+
+
+function setupCalendar() {
+
   $('#calendar').fullCalendar({
     // put your options and callbacks here
 
@@ -18,6 +43,33 @@ $(function() {
       and have a link to create a reservation
     */
     dayClick: function(date, allDay, jsEvent, view) {
+
+      $("#sample").modal({
+        minHeight: 2000,
+        minWidth: 600,
+
+        onOpen: function (dialog) {
+          dialog.overlay.fadeIn('slow', function () {
+            dialog.data.hide();
+            dialog.container.fadeIn('slow', function () {
+              dialog.data.slideDown('slow');	 
+              $('#modal-calendar').fullCalendar({
+                defaultView: 'agendaDay',
+              });
+
+            });
+          });
+        },
+
+        position: [50,50],
+
+      });
+
+    },
+
+
+
+    dayClickOld: function(date, allDay, jsEvent, view) {
       /* note: 'this' refers to the <td> of the clicked day */
 
       /* check for and remove old popover */
@@ -59,78 +111,24 @@ $(function() {
       $('#cal-popover').popover('show');
       setClose(); /* set up event to fire when popover's X box is clicked */
 
-
-
-      //$(this).css('background', 'url(../images/turtle.jpg)');
-
-
-
-
-      /* save this 
-      $("#sample").modal({
-        minHeight: 2000,
-        minWidth: 600,
-        //onClose: function() {
-          //alert('sup');
-        //},
-
-        onOpen: function (dialog) {
-          dialog.overlay.fadeIn('slow', function () {
-            dialog.data.hide();
-            dialog.container.fadeIn('slow', function () {
-              dialog.data.slideDown('slow');	 
-              $('#modal-calendar').fullCalendar({
-                defaultView: 'agendaDay',
-              });
-
-            });
-          });
-        },
-
-        position: [50,50],
-
-
-      });
-      */
-
     },
 
 
+    /*
     eventMouseover: function(event, jsEvent, view) {
       $.getJSON('Dash', function(response) {
         alert(response.names[0]);   // john doe
       });
     },
+    */
 
     // example of events
-
     events: [
         {
             title: 'an event',
             start: '2012-10-16',
             description: 'This is a cool event'
-        },/*{
-            title: 'My Event',
-            start: '2012-10-10',
-            description: 'This is a cool event'
-        },{
-            title: 'My Event',
-            start: '2012-10-10',
-            description: 'This is a cool event'
-        },{
-            title: 'My Event',
-            start: '2012-10-10',
-            description: 'This is a cool event'
-        },{
-            title: 'My Event',
-            start: '2012-10-10',
-            description: 'This is a cool event'
-        },{
-            title: 'My Event',
-            start: '2012-10-10',
-            description: 'This is a cool event'
-        }*/
-
+        }
     ],
 
     // example of how to move the header
@@ -142,21 +140,9 @@ $(function() {
 
   })
 
- 
-  /*
-  $("#close-popover").on("click", function(event) {
-    alert('closer');
-  })
-  */
 
-  
+}
 
-
-});
-
-
-
-// DECLARE FUNCTIONS HERE
 
 function onIntChange() {
   //$('#randomDiv').html('you chose' + $(this).val());
@@ -187,3 +173,51 @@ function killPopover(t) {
 
 
 
+
+function enablePopover() {
+
+  $(".fc-widget-content").on('hover', function() {
+
+    /* check for and remove old popover */
+    var t = $('#cal-popover');
+    if (t.length != 0) {
+      killPopover(t);
+    }
+      
+    /*
+      grab the appropriate td.
+      fullcalendar doesn't play nice with positioning bootstrap's popovers.
+      the best combination seems to be:
+        anything sunday through wednesday, use the NEXT td and position to the right
+        anything thursday through saturday, use the CLICKED td and position to the left
+      this keeps the popover's bubble pointing to the appropriate day, and it keeps
+      it from spilling beyond the window's width
+    */
+    //var day = (''+date).substring(0,3);
+    var popPosition = "left";
+    $(this).append('<a href="#" id="cal-popover" rel="popover"></a>');
+    /*if (day == 'Thu' || day == 'Fri' || day == 'Sat') {
+      popPosition = 'left';
+      $(this).append('<a href="#" id="cal-popover" rel="popover"></a>');
+    }
+    else {
+      popPosition = 'right';
+      $(this).next().append('<a href="#" id="cal-popover" rel="popover"></a>');
+    }*/
+
+    /* initialize other popover vars */
+    var popTitle = 'meh title  <a href="#" id="close-popover" style="float:right;">X</a>';
+    var popContent = '<h1>h1 content </h1><p>p tag</p>';
+
+    /* show the popover */
+    $("#cal-popover").popover({
+      placement: popPosition,
+      title: popTitle,
+      content: popContent,
+    });
+    $('#cal-popover').popover('show');
+    setClose(); /* set up event to fire when popover's X box is clicked */
+
+  });
+
+}
