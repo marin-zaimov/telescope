62fda9e84c032c328b67d68cf1f6922ba109272b
Jasmine Lawrence <jasmine@jasmine-900X3C-900X4C-900X4D.(none)>
2012-10-28 12:49:23 -0400

    updated the about us page and telescope page to include video and new text

--- maui/application/protected/views/user/account.php
+++ maui/application/protected/views/user/account.php
@@ -25,8 +25,6 @@ Saturn - Monday June, 2013 at 5:00pm EST</p>
 <p><b>VIDEOS</b></p>
 <br>
 
-<embed src="http://www.tizag.com/files/html/htmlexample.mpeg"
- autostart="false" />
  
 <iframe width="640" height="360" src="http://www.youtube.com/embed/qa04iRjaBMA?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>
 
